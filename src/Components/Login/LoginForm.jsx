f2c13cb34748d6185cf8c319ba50a86268a0b782
unknown <fabioflauzino@hotmail.com>
2023-10-05 23:34:32 -0300

    componentizando formulário e criando hook de formulário

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const username = useForm()
+  const password = useForm()
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault()
+
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify()
+      })
+        .then(res => res.json())
+        .then(json => console.log(json))
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={({target}) => setUsername(target.value)}/>
-        <input type="password" value={password} onChange={({target}) => setPassword(target.value)}/>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
