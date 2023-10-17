4d8540cba26d8d93233f2edb4617b52caf3e941d
Fabio Flauzino <fabio@mfrural.com.br>
2023-10-17 18:33:24 -0300

    Iniciando o uso de Context

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
-import './App.css'
+import "./App.css";
+import { UserStorage } from "./UserContext";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
+        <UserStorage>
+          <Header />
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/login/*" element={<Login />}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
           </Routes>
-        <Footer />
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
