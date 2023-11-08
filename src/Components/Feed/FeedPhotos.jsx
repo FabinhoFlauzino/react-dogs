25943743ec1d53b6a981f33672c7feb8af3f323a
Fabio Flauzino <fabio@mfrural.com.br>
2023-11-08 18:03:10 -0300

    Inicinado Feed Modal

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,49 +1,52 @@
-import React, { useEffect } from 'react'
-import FeedPhotosItem from './FeedPhotosItem'
-import useFetch from '../../Hooks/useFetch'
-import { PHOTOS_GET } from '../../api'
-import Error from '../Helper/Error'
-import Loading from '../Helper/Loading'
-import styles from './FeedPhotos.module.css'
+import React, { useEffect } from "react";
+import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+import styles from "./FeedPhotos.module.css";
 
-
-const FeedPhotos = () => {
-
-  const { data, loading, error, request } = useFetch()
+const FeedPhotos = ({ setModalPhoto }) => {
+  const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({
         page: 1,
         total: 6,
-        user: 0
-      })
+        user: 0,
+      });
 
-      const { response, json } = await request(url, options)
+      const { response, json } = await request(url, options);
       console.log(json);
     }
 
-    fetchPhotos()
-  }, [request])
+    fetchPhotos();
+  }, [request]);
 
   if (error) {
-    return <Error error={error} />
+    return <Error error={error} />;
   }
 
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
 
   if (data) {
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo} />)}
+        {data.map((photo) => (
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
       </ul>
-    )
+    );
   } else {
-    return null
+    return null;
   }
+};
 
-}
-
-export default FeedPhotos
+export default FeedPhotos;
